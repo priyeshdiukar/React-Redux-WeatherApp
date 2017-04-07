b1ca7287569d278dd408dae6046d8ba5dd0a230b
priyesh <priyesh@pivotroots.com>
2017-04-07 17:20:37 +0530

    first working commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = "247560072590b98e70eeff920fe1065a";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return{
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
